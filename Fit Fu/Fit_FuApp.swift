abddf4497726f33e7bf898093ae1c6d5d99cd095
Amir Abravesh <amir.abravesh.ama@gmail.com>
2024-05-14 14:35:23 +0330

    add copyright notice

--- Fit Fu/Fit_FuApp.swift
+++ Fit Fu/Fit_FuApp.swift
@@ -1,9 +1,18 @@
+// Fit Fu
+// Copyright (C) 2024 Amir Mahdi Abravesh and Ilam University
 //
-//  Fit_FuApp.swift
-//  Fit Fu
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
 //
-//  Created by Amir Mahdi Abravesh on 5/14/24.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
+// You should have received a copy of the GNU General Public License along
+// with this program;
 
 import SwiftUI
 
