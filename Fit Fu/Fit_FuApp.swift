c3a479a73482f5e86082caaa0963797876084434
Amir Abravesh <amir.abravesh.ama@gmail.com>
2024-05-14 14:12:55 +0330

    initial commit

---
+++ Fit Fu/Fit_FuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fit_FuApp.swift
+//  Fit Fu
+//
+//  Created by Amir Mahdi Abravesh on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Fit_FuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
